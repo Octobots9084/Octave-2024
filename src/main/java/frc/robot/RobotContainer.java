656b1f7deb8086a43e23bde942c586a41bc92ed9
Coolkid0629 <90662954+Coolkid0629@users.noreply.github.com>
2024-01-08 20:52:13 -0800

    Meeting 1 after kickoff, added auto code and fixed driving untested

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,17 +4,33 @@
 
 package frc.robot;
 
+import java.io.File;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+import com.pathplanner.lib.path.PathPlannerPath;
+
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.swerve.SwerveSubsystem;
 
 public class RobotContainer {
-  public RobotContainer() {
-    configureBindings();
+  private final SendableChooser<Command> autoChooser;
+  private final SwerveSubsystem subsystemBase = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(),
+  "swerve"));
+
+  public RobotContainer() {    
+    autoChooser = AutoBuilder.buildAutoChooser();
+    SmartDashboard.putData("Auto Chooser", autoChooser);
   }
 
   private void configureBindings() {}
 
   public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
+    PathPlannerPath path = PathPlannerPath.fromPathFile("1m");
+    return AutoBuilder.followPath(path);
   }
 }
