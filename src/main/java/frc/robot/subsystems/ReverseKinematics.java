6535c61fa1cb016b44037907df13f0752821af2a
nateVanDeVeire <bluefury6@gmail.com>
2024-01-15 19:44:27 -0800

    updated reverse kinematics - not finished yet

--- src/main/java/frc/robot/subsystems/ReverseKinematics.java
+++ src/main/java/frc/robot/subsystems/ReverseKinematics.java
@@ -1,25 +1,29 @@
 package frc.robot.subsystems;
 
+// note: only works on the left side of the target
+
 public class ReverseKinematics {
     // all units should be in meters, m/s, rad, etc.
 
     // distance between launcher opening and target height
-    private static double constTargetHeightDiff = 1;
+    private static double constTargetHeightDiff = 13;
     // gravity
     private static double g = -9.8;
 
     public static double calcLaunchVerticalVel() {
-        return Math.sqrt(constTargetHeightDiff * g * g);
+        return -g*Math.sqrt(Math.abs(2*constTargetHeightDiff/g)); //Math.sqrt(constTargetHeightDiff * g * g);
     }
 
     // velocities are positive going towards the target and negative when moving away
 
     public static double calcLaunchXVel(double xPos, double xVel) {
-        return xPos/(constTargetHeightDiff*calcLaunchVerticalVel()) - xVel;
+        System.out.println("launch x velocity: " + (xPos/(constTargetHeightDiff/calcLaunchVerticalVel()) - xVel));
+        return xPos/(2*constTargetHeightDiff/calcLaunchVerticalVel()) - xVel;
     }
 
     public static double calcLaunchYVel(double yPos, double yVel) {
-        return yPos/(constTargetHeightDiff*calcLaunchVerticalVel()) - yVel;
+        System.out.println("launch y velocity: " + (yPos/(constTargetHeightDiff/calcLaunchVerticalVel()) - yVel));
+        return yPos/(2*constTargetHeightDiff/calcLaunchVerticalVel()) - yVel;
     }
 
     public static double calcTotalLaunchVelocity(double xPos, double yPos, double xVel, double yVel) {
@@ -34,6 +38,6 @@ public class ReverseKinematics {
     // xPos is the distance from being in line with the amp
     // yPos is distance from the amp (including the extended base part of it)
     public static double calcAmpLaunchAngle(double xPos, double yPos, double xVel, double yVel) {
-        return Math.atan(Math.sqrt(Math.pow(calcLaunchXVel(xPos, xVel), 2) + Math.pow(calcLaunchYVel(yPos, yVel), 2))/calcLaunchVerticalVel());
+        return Math.atan(calcLaunchVerticalVel()/Math.sqrt(Math.pow(calcLaunchXVel(xPos, xVel), 2) + Math.pow(calcLaunchYVel(yPos, yVel), 2)));
     }
 }
