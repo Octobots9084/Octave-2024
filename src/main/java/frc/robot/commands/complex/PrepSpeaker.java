781bbe517316669fd180f50736228dcd2534794d
Petar Genov <petar.genov.2008@gmail.com>
2024-01-17 20:16:37 -0800

    Speaker and Amp buttons set

---
+++ src/main/java/frc/robot/commands/complex/PrepSpeaker.java
@@ -0,0 +1,19 @@
+package frc.robot.commands.complex;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.arm.ShooterElevatorPosTolerance;
+import frc.robot.commands.arm.ShooterFlywheelSpeedInstant;
+import frc.robot.commands.arm.ShooterPivotPosInstant;
+import frc.robot.constants.ArmPositions;
+import frc.robot.constants.ShooterSpeeds;
+
+public class PrepSpeaker extends SequentialCommandGroup{
+    public PrepSpeaker(){
+        addCommands(
+            new ShooterFlywheelSpeedInstant(ShooterSpeeds.SPEAKER),
+            new ShooterElevatorPosTolerance(ArmPositions.HANDOFF_AND_DEFAULT_SHOT), 
+            new ShooterPivotPosInstant(ArmPositions.HANDOFF_AND_DEFAULT_SHOT)
+            
+        );
+    }
+}
