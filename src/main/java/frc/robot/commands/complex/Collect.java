b7baca1b2a00bcfbe367459458aef91f4759876e
Anemone <zombiegam3r2o06@gmail.com>
2024-01-17 19:49:11 -0800

    Co-authored-by: Lauren Lee <LaurenYYLee@users.noreply.github.com> Co-authored-by: Petar52 <Petar52@users.noreply.github.com> Co-authored-by: Richardsteele111 <Richardsteele111@users.noreply.github.com>

---
+++ src/main/java/frc/robot/commands/complex/Collect.java
@@ -0,0 +1,12 @@
+package frc.robot.commands.complex;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+
+public class Collect extends SequentialCommandGroup{
+    public Collect() {
+        addCommands(
+            new 
+        );
+    }
+}
