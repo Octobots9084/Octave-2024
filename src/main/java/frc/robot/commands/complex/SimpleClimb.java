52d3d98b5b9b3eb39a16f4855a06da5e2ec20220
lyyleem <laurenlee4655@gmail.com>
2024-01-19 14:25:03 -0800

    prep + simple climb

--- src/main/java/frc/robot/commands/complex/SimpleClimb.java
+++ src/main/java/frc/robot/commands/complex/SimpleClimb.java
@@ -1,16 +1,12 @@
 package frc.robot.commands.complex;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.climb.ClimbPosTolerance;
 import frc.robot.constants.ClimbPositions;
 
 public class SimpleClimb extends SequentialCommandGroup{
     public SimpleClimb() {
         addCommands(
-            new ClimbPosTolerance(ClimbPositions.DOWN).withTimeout(1),
-            new ClimbPosTolerance(ClimbPositions.UP).withTimeout(2),
-            new WaitCommand(3),
             new ClimbPosTolerance(ClimbPositions.DOWN));
     }
 }
