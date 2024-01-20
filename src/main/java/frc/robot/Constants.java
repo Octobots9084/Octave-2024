2f3d47e8575ee3aa2840d5ad205d892b8fb3ac38
Aayush Kokate <aayushext@outlook.com>
2024-01-20 11:56:51 -0800

    No error vision with yagsl but no pose estimator yet

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,7 +4,14 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.MatBuilder;
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.Nat;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.util.Units;
 import swervelib.math.Matter;
 import swervelib.parser.PIDFConfig;
@@ -48,5 +55,89 @@ public final class Constants {
     public static final double LEFT_Y_DEADBAND = 0.01;
     public static final double RIGHT_X_DEADBAND = 0.01;
     public static final double TURN_CONSTANT = 6;
+  }
+
+  public static class FieldConstants {
+    public static final double length = Units.feetToMeters(54);
+    public static final double width = Units.feetToMeters(27);
+  }
+
+  public static final class VisionConstants {
+
+    public static boolean USE_VISION = true;
+
+    /**
+     * Physical location of the front right camera on the robot, relative to the center of
+     * the
+     * robot.
+     */
+    public static final Transform3d ROBOT_TO_FRONT_RIGHT_CAMERA = new Transform3d(
+        new Translation3d(Units.inchesToMeters(-11), Units.inchesToMeters(-11),
+            Units.inchesToMeters(22.5)),
+        new Rotation3d(0, Math.toRadians(0), Math.toRadians(0)));
+
+    /**
+     * Physical location of the front left camera on the robot, relative to the center of
+     * the
+     * robot.
+     */
+    public static final Transform3d ROBOT_TO_FRONT_LEFT_CAMERA = new Transform3d(
+        new Translation3d(Units.inchesToMeters(11), Units.inchesToMeters(-11),
+            Units.inchesToMeters(22.5)),
+        new Rotation3d(0, Math.toRadians(0), Math.toRadians(0)));
+
+    /**
+     * Physical location of the back right camera on the robot, relative to the center of
+     * the
+     * robot.
+     */
+    public static final Transform3d ROBOT_TO_BACK_RIGHT_CAMERA = new Transform3d(
+        new Translation3d(Units.inchesToMeters(-13.76), Units.inchesToMeters(6.1),
+            Units.inchesToMeters(33.65)),
+        new Rotation3d(0, Math.toRadians(10.62), Math.toRadians(180)));
+
+    /**
+     * Physical location of the back left camera on the robot, relative to the center of
+     * the
+     * robot.
+     */
+    public static final Transform3d ROBOT_TO_BACK_LEFT_CAMERA = new Transform3d(
+        new Translation3d(Units.inchesToMeters(-13.76), Units.inchesToMeters(6.1),
+            Units.inchesToMeters(33.65)),
+        new Rotation3d(0, Math.toRadians(10.62), Math.toRadians(180)));
+
+    /** Minimum target ambiguity. Targets with higher ambiguity will be discarded */
+    public static final double APRILTAG_AMBIGUITY_THRESHOLD = 0.2;
+    public static final double POSE_AMBIGUITY_SHIFTER = 0.2;
+    public static final double POSE_AMBIGUITY_MULTIPLIER = 4;
+    public static final double NOISY_DISTANCE_METERS = 2.5;
+    public static final double DISTANCE_WEIGHT = 7;
+    public static final int TAG_PRESENCE_WEIGHT = 10;
+
+    /**
+     * Standard deviations of model states. Increase these numbers to trust your
+     * model's state estimates less. This
+     * matrix is in the form [x, y, theta]ᵀ, with units in meters and radians, then
+     * meters.
+     * If these numbers are less than one, multiplying will do bad things
+     */
+
+    public static final Matrix<N3, N1> VISION_MEASUREMENT_STANDARD_DEVIATIONS = MatBuilder.fill(Nat.N3(),
+        Nat.N1(), 1, // x
+        1, // y
+        1 * Math.PI); // theta
+
+    /**
+     * Standard deviations of the vision measurements. Increase these numbers to
+     * trust global measurements from vision
+     * less. This matrix is in the form [x, y, theta]ᵀ, with units in meters and
+     * radians.
+     * If these numbers are less than one, multiplying will do bad things
+     */
+    public static final Matrix<N3, N1> STATE_STANDARD_DEVIATIONS = MatBuilder.fill(Nat.N3(), Nat.N1(),
+        .1, // x
+        .1, // y 
+        .1);
+
   }
 }
---
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -0,0 +1,89 @@
+package frc.robot.subsystems.vision;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.photonvision.EstimatedRobotPose;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout.OriginPosition;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.wpilibj.DriverStation;
+import frc.robot.Constants.VisionConstants;
+import frc.robot.Constants.FieldConstants;;
+
+/**
+ * Runnable that gets AprilTag data from PhotonVision.
+ */
+public class Vision implements Runnable {
+
+  private final PhotonPoseEstimator photonPoseEstimator;
+  private final PhotonCamera photonCamera;
+  private final AtomicReference<EstimatedRobotPose> atomicEstimatedRobotPose = new AtomicReference<EstimatedRobotPose>();
+
+  public Vision(PhotonCamera cameraName, Transform3d robotToCamera) {
+    this.photonCamera = cameraName;
+    PhotonPoseEstimator photonPoseEstimator = null;
+    try {
+      var layout = AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
+      layout.setOrigin(OriginPosition.kBlueAllianceWallRightSide); // Sets the Origin to the Blue AllianceWall and will be flipped in the robot thread
+
+      // if (photonCamera != null) {
+      photonPoseEstimator = photonCamera != null
+          ? new PhotonPoseEstimator(layout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, photonCamera, robotToCamera)
+          : null;
+      photonPoseEstimator.setMultiTagFallbackStrategy(PoseStrategy.LOWEST_AMBIGUITY);
+      // }
+    } catch (Exception e) {
+      DriverStation.reportError("Failed to load AprilTagFieldLayout", e.getStackTrace());
+      // photonPoseEstimator = null;
+    }
+
+    this.photonPoseEstimator = photonPoseEstimator;
+  }
+
+  @Override
+  public void run() {
+    // Get AprilTag data and updating the pose estimator
+    try {
+      if (photonPoseEstimator != null && photonCamera != null) {
+        var photonResults = photonCamera.getLatestResult(); //Gets the latest camera results
+        if (photonResults.hasTargets() && (photonResults.targets.size() > 1
+            || photonResults.targets.get(0).getPoseAmbiguity() < VisionConstants.APRILTAG_AMBIGUITY_THRESHOLD)) {
+          //Updates the pose estimator
+          photonPoseEstimator.update(photonResults).ifPresent(estimatedRobotPose -> {
+            var estimatedPose = estimatedRobotPose.estimatedPose;
+            /** 
+            * If present then makes sure the measurement is on the field and
+            * sets the atomic estimated pose to the current estimated pose
+            * from PhotonPoseEstimator
+            */
+            if (estimatedPose.getX() > 0.0 && estimatedPose.getX() <= FieldConstants.length
+                && estimatedPose.getY() > 0.0
+                && estimatedPose.getY() <= FieldConstants.width) {
+              atomicEstimatedRobotPose.set(estimatedRobotPose);
+            }
+          });
+        }
+      }
+    } catch (Exception e) {
+      // TODO: handle exception
+    }
+  }
+
+  /**
+   * Gets the latest robot pose. Calling this will only return the pose once. If
+   * it returns a non-null value, it is a
+   * new estimate that hasn't been returned before.
+   * This pose will always be for the BLUE alliance. It must be flipped if the
+   * current alliance is RED.
+   * 
+   * @return latest estimated pose
+   */
+  public EstimatedRobotPose grabLatestEstimatedPose() {
+    return atomicEstimatedRobotPose.get();
+  }
+
+}
