c972eeecb73668e7be8ebcdd380fe9fcc8639fb5
Aayush Kokate <aayushext@outlook.com>
2024-01-20 11:50:41 -0800

    Imported YAGSL

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,130 +1,52 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot;
 
-import java.util.List;
-import java.util.Map;
-
-import edu.wpi.first.math.MatBuilder;
-import edu.wpi.first.math.Matrix;
-import edu.wpi.first.math.Nat;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.numbers.N1;
-import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.util.Units;
+import swervelib.math.Matter;
+import swervelib.parser.PIDFConfig;
 
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This
+ * class should not be used for any other purpose. All constants should be
+ * declared globally (i.e. public static). Do
+ * not put anything functional in this class.
+ *
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
+ * constants are needed, to reduce verbosity.
+ */
 public final class Constants {
 
-        // public static final class DrivetrainConstants {
-        //         /**
-        //         * The left-to-right distance between the drivetrain wheels
-        //         * <p>
-        //         * Should be measured from center to center.
-        //         */
-        //         public static final double DRIVETRAIN_TRACKWIDTH_METERS = 0.3556;
-        //         /**
-        //          * The front-to-back distance between the drivetrain wheels.
-        //          * <p>
-        //          * Should be measured from center to center.
-        //          */
-        //         public static final double DRIVETRAIN_WHEELBASE_METERS = 0.3556;
+  public static final double ROBOT_MASS = (65) * 0.453592; // 32lbs * kg per pound
+  public static final Matter CHASSIS = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
+  public static final double LOOP_TIME = 0.13; // s, 20ms + 110ms sprk max velocity lag
 
-        //         public static final SwerveDriveKinematics KINEMATICS = new SwerveDriveKinematics(
-        //                         // Front left
-        //                         new Translation2d(DRIVETRAIN_TRACKWIDTH_METERS / 2.0,
-        //                                         DRIVETRAIN_WHEELBASE_METERS / 2.0),
-        //                         // Front right
-        //                         new Translation2d(DRIVETRAIN_TRACKWIDTH_METERS / 2.0,
-        //                                         -DRIVETRAIN_WHEELBASE_METERS / 2.0),
-        //                         // Back left
-        //                         new Translation2d(-DRIVETRAIN_TRACKWIDTH_METERS / 2.0,
-        //                                         DRIVETRAIN_WHEELBASE_METERS / 2.0),
-        //                         // Back right
-        //                         new Translation2d(-DRIVETRAIN_TRACKWIDTH_METERS / 2.0,
-        //                                         -DRIVETRAIN_WHEELBASE_METERS / 2.0));
-        // }
+  public static final class Auton {
 
-        public static final class VisionConstants {
-                public static final double FIELD_LENGTH_METERS = Units.inchesToMeters(651.25);
-                public static final double FIELD_WIDTH_METERS = Units.inchesToMeters(315.5);
+    public static final PIDFConfig TranslationPID = new PIDFConfig(0.7, 0, 0);
+    public static final PIDFConfig angleAutoPID = new PIDFConfig(0.4, 0, 0.01);
 
-                public static boolean USE_VISION = true;
+    public static final double MAX_ACCELERATION = 2;
+  }
 
-                /**
-                 * Physical location of the front right camera on the robot, relative to the center of
-                 * the
-                 * robot.
-                 */
-                public static final Transform3d ROBOT_TO_FRONT_RIGHT_CAMERA = new Transform3d(
-                                new Translation3d(Units.inchesToMeters(-11.88), Units.inchesToMeters(-6.88),
-                                                Units.inchesToMeters(31.09)),
-                                new Rotation3d(0, Math.toRadians(10.62), Math.toRadians(-45)));
+  public static final class Drivebase {
 
-                /**
-                 * Physical location of the front left camera on the robot, relative to the center of
-                 * the
-                 * robot.
-                 */
-                public static final Transform3d ROBOT_TO_FRONT_LEFT_CAMERA = new Transform3d(
-                                new Translation3d(Units.inchesToMeters(-11.88), Units.inchesToMeters(6.88),
-                                                Units.inchesToMeters(31.09)),
-                                new Rotation3d(0, Math.toRadians(10.62), Math.toRadians(46)));
+    // Hold time on motor brakes when disabled
+    public static final double WHEEL_LOCK_TIME = 10; // seconds
+  }
 
-                /**
-                 * Physical location of the back right camera on the robot, relative to the center of
-                 * the
-                 * robot.
-                 */
-                public static final Transform3d ROBOT_TO_BACK_RIGHT_CAMERA = new Transform3d(
-                                new Translation3d(Units.inchesToMeters(-13.76), Units.inchesToMeters(6.1),
-                                                Units.inchesToMeters(33.65)),
-                                new Rotation3d(0, Math.toRadians(10.62), Math.toRadians(180)));
+  public static class OperatorConstants {
 
-                /**
-                 * Physical location of the back left camera on the robot, relative to the center of
-                 * the
-                 * robot.
-                 */
-                public static final Transform3d ROBOT_TO_BACK_LEFT_CAMERA = new Transform3d(
-                                new Translation3d(Units.inchesToMeters(-13.76), Units.inchesToMeters(6.1),
-                                                Units.inchesToMeters(33.65)),
-                                new Rotation3d(0, Math.toRadians(10.62), Math.toRadians(180)));
-
-                /** Minimum target ambiguity. Targets with higher ambiguity will be discarded */
-                public static final double APRILTAG_AMBIGUITY_THRESHOLD = 0.2;
-                public static final double POSE_AMBIGUITY_SHIFTER = 0.2;
-                public static final double POSE_AMBIGUITY_MULTIPLIER = 4;
-                public static final double NOISY_DISTANCE_METERS = 2.5;
-                public static final double DISTANCE_WEIGHT = 7;
-                public static final int TAG_PRESENCE_WEIGHT = 10;
-
-                /**
-                 * Standard deviations of model states. Increase these numbers to trust your
-                 * model's state estimates less. This
-                 * matrix is in the form [x, y, theta]ᵀ, with units in meters and radians, then
-                 * meters.
-                 * If these numbers are less than one, multiplying will do bad things
-                 */
-
-                public static final Matrix<N3, N1> VISION_MEASUREMENT_STANDARD_DEVIATIONS = MatBuilder.fill(Nat.N3(),
-                                Nat.N1(), 1, // x
-                                1, // y
-                                1 * Math.PI); // theta
-
-                /**
-                 * Standard deviations of the vision measurements. Increase these numbers to
-                 * trust global measurements from vision
-                 * less. This matrix is in the form [x, y, theta]ᵀ, with units in meters and
-                 * radians.
-                 * If these numbers are less than one, multiplying will do bad things
-                 */
-                public static final Matrix<N3, N1> STATE_STANDARD_DEVIATIONS = MatBuilder.fill(Nat.N3(), Nat.N1(),
-                                .1, // x
-                                .1, // y 
-                                .1);
-        }
+    // Joystick Deadband
+    public static final double LEFT_X_DEADBAND = 0.01;
+    public static final double LEFT_Y_DEADBAND = 0.01;
+    public static final double RIGHT_X_DEADBAND = 0.01;
+    public static final double TURN_CONSTANT = 6;
+  }
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,97 +4,103 @@
 
 package frc.robot;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-
-import org.photonvision.PhotonCamera;
-import org.photonvision.estimation.VisionEstimation;
-
-import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.interfaces.Gyro;
-import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.Constants.VisionConstants;
-import frc.robot.subsystems.PoseEstimator;
-import frc.robot.subsystems.VisionRunnable;
+import java.io.File;
+import java.io.IOException;
+import swervelib.parser.SwerveParser;
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as
+ * described in the TimedRobot documentation. If you change the name of this
+ * class or the package after creating this
+ * project, you must also update the build.gradle file in the project.
  */
 public class Robot extends TimedRobot {
+
+  private static Robot instance;
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
 
-  public final VisionRunnable frontRightEstimator = new VisionRunnable(new PhotonCamera("frontRightCamera"),
-      VisionConstants.ROBOT_TO_FRONT_LEFT_CAMERA);
-  public final VisionRunnable frontLeftEstimator = new VisionRunnable(new PhotonCamera("frontLeftCamera"),
-      VisionConstants.ROBOT_TO_FRONT_LEFT_CAMERA);
-  public final VisionRunnable backRightEstimator = new VisionRunnable(new PhotonCamera("backRightCamera"),
-      VisionConstants.ROBOT_TO_BACK_RIGHT_CAMERA);
-  public final VisionRunnable backLeftEstimator = new VisionRunnable(new PhotonCamera("backLeftCamera"),
-      VisionConstants.ROBOT_TO_BACK_LEFT_CAMERA);
+  private Timer disabledTimer;
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
-  @Override
-  public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
-    m_robotContainer = new RobotContainer();
+  public Robot() {
+    instance = this;
+  }
 
-    // visionThread = new Thread(vision);
-
-    // visionThread.start();
+  public static Robot getInstance() {
+    return instance;
   }
 
   /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
-   * that you want ran during disabled, autonomous, teleoperated and test.
+   * This function is run when the robot is first started up and should be used
+   * for any initialization code.
+   */
+  @Override
+  public void robotInit() {
+    // Instantiate our RobotContainer. This will perform all our button bindings,
+    // and put our
+    // autonomous chooser on the dashboard.
+    m_robotContainer = new RobotContainer();
+
+    // Create a timer to disable motor brake a few seconds after disable. This will
+    // let the robot stop
+    // immediately when disabled, but then also let it be pushed more
+    disabledTimer = new Timer();
+  }
+
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items
+   * like diagnostics that you want ran
+   * during disabled, autonomous, teleoperated and test.
    *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and
    * SmartDashboard integrated updating.
    */
   @Override
   public void robotPeriodic() {
+    // Runs the Scheduler. This is responsible for polling buttons, adding
+    // newly-scheduled
+    // commands, running already-scheduled commands, removing finished or
+    // interrupted commands,
+    // and running subsystem periodic() methods. This must be called from the
+    // robot's periodic
+    // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
-    // System.out.print(visionThread.isAlive());
-    SmartDashboard.putNumber("Front X Difference",
-        frontRightEstimator.grabLatestEstimatedPose().estimatedPose.toPose2d().getX()
-            - frontLeftEstimator.grabLatestEstimatedPose().estimatedPose.toPose2d().getX());
-    SmartDashboard.putNumber("Front Y Difference",
-        frontRightEstimator.grabLatestEstimatedPose().estimatedPose.toPose2d().getY()
-            - frontLeftEstimator.grabLatestEstimatedPose().estimatedPose.toPose2d().getY());
-    // SmartDashboard.putNumber("X1", vision.getCurrentPoseOne().getX());
-    // SmartDashboard.putNumber("y1", vision.getCurrentPoseOne().getY());
-    // SmartDashboard.putNumber("z1", vision.getCurrentPoseOne().getZ());
-    // SmartDashboard.putNumber("X2", vision.getCurrentPoseTwo().getX());
-    // SmartDashboard.putNumber("Difference Y", vision.getCurrentPoseOne().getY() - vision.getCurrentPoseTwo().getY());
-    // SmartDashboard.putNumber("Difference Z", vision.getCurrentPoseOne().getZ() - vision.getCurrentPoseTwo().getZ());
   }
 
-  /** This function is called once each time the robot enters Disabled mode. */
+  /**
+   * This function is called once each time the robot enters Disabled mode.
+   */
   @Override
   public void disabledInit() {
+    m_robotContainer.setMotorBrake(true);
+    disabledTimer.reset();
+    disabledTimer.start();
   }
 
   @Override
   public void disabledPeriodic() {
+    if (disabledTimer.hasElapsed(Constants.Drivebase.WHEEL_LOCK_TIME)) {
+      m_robotContainer.setMotorBrake(false);
+      disabledTimer.stop();
+    }
   }
 
-  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  /**
+   * This autonomous runs the autonomous command selected by your
+   * {@link RobotContainer} class.
+   */
   @Override
   public void autonomousInit() {
+    m_robotContainer.setMotorBrake(true);
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
     // schedule the autonomous command (example)
@@ -103,7 +109,9 @@ public class Robot extends TimedRobot {
     }
   }
 
-  /** This function is called periodically during autonomous. */
+  /**
+   * This function is called periodically during autonomous.
+   */
   @Override
   public void autonomousPeriodic() {
   }
@@ -117,10 +125,13 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
-
+    m_robotContainer.setDriveMode();
+    m_robotContainer.setMotorBrake(true);
   }
 
-  /** This function is called periodically during operator control. */
+  /**
+   * This function is called periodically during operator control.
+   */
   @Override
   public void teleopPeriodic() {
   }
@@ -129,19 +140,30 @@ public class Robot extends TimedRobot {
   public void testInit() {
     // Cancels all running commands at the start of test mode.
     CommandScheduler.getInstance().cancelAll();
+    try {
+      new SwerveParser(new File(Filesystem.getDeployDirectory(), "swerve"));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 
-  /** This function is called periodically during test mode. */
+  /**
+   * This function is called periodically during test mode.
+   */
   @Override
   public void testPeriodic() {
   }
 
-  /** This function is called once when the robot is first started up. */
+  /**
+   * This function is called once when the robot is first started up.
+   */
   @Override
   public void simulationInit() {
   }
 
-  /** This function is called periodically whilst in simulation. */
+  /**
+   * This function is called periodically whilst in simulation.
+   */
   @Override
   public void simulationPeriodic() {
   }
