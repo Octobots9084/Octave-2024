96c9e1f4cf032a5e5e044451b75e98990fb7022f
OlorinOut <sebastian@fedaykin.net>
2024-01-24 19:04:46 -0800

    NIGHTMARE NIGHTMARE NIGHTMARE

--- src/main/java/frc/robot/commands/complex/Driveby.java
+++ src/main/java/frc/robot/commands/complex/Driveby.java
@@ -5,6 +5,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.ReverseKinematics;
 import frc.robot.subsystems.ShooterFlywheel;
 import frc.robot.subsystems.ShooterPivot;
 import frc.robot.subsystems.swervedrive.SwerveSubsystem;
@@ -30,9 +31,11 @@ public class Driveby extends Command{
     }
 
     public void updateTargets(){
-        targetPivot = 0;
-        targetFlywheel = 0;
-        targetTurn = new Rotation2d();
+        realPose2d = SwerveSubsystem.getInstance().getPose();
+        realSpeeds = SwerveSubsystem.getInstance().getFieldVelocity();
+        targetPivot = ReverseKinematics.calcSubwooferLaunchAngle(realPose2d, realSpeeds);
+        targetFlywheel = ReverseKinematics.calcTotalLaunchVelocity(realPose2d, realSpeeds);
+        targetTurn = new Rotation2d(ReverseKinematics.calcRobotAngle(realPose2d, realSpeeds));
     }
     
     @Override
