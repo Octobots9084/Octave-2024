42e6419ea93bed746b3aa47eb4ab3defa25fd72c
AlexanderGardiner <misc@alexandergardiner.com>
2024-02-19 10:17:06 -0800

    Jiggle note finishes at correct spot

--- src/main/java/frc/robot/commands/complex/Collect.java
+++ src/main/java/frc/robot/commands/complex/Collect.java
@@ -60,7 +60,7 @@ public class Collect extends SequentialCommandGroup {
                 }),
                 new ShooterTrackSpeedInstant(ShooterSpeeds.STOP),
                 new IntakeTrackSpeedInstant(IntakeSpeeds.STOP),
-                new JiggleNote().withTimeout(2.5));
+                new JiggleNote(2.5));
 
     }
 
