60abb7d03e3ac484f2b695829ed4030e673efae2
AlexanderGardiner <misc@alexandergardiner.com>
2024-03-03 13:23:17 -0800

    So close

--- src/main/java/frc/robot/commands/complex/InitalSpeakerAutoFast.java
+++ src/main/java/frc/robot/commands/complex/InitalSpeakerAutoFast.java
@@ -3,6 +3,7 @@ package frc.robot.commands.complex;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.arm.ShooterFlywheelPercentInstant;
+import frc.robot.commands.arm.ShooterFlywheelSpeedInstantMeters;
 import frc.robot.commands.arm.ShooterFlywheelSpeedToleranceMeters;
 import frc.robot.commands.arm.ShooterPivotPosInstant;
 import frc.robot.commands.arm.ShooterTrackPercentInstant;
@@ -13,7 +14,7 @@ public class InitalSpeakerAutoFast extends SequentialCommandGroup {
     public InitalSpeakerAutoFast() {
 
         addCommands(new ShooterPivotPosInstant(ArmPositions.SPEAKER_SHOT), new WaitCommand(0),
-                new ShooterFlywheelSpeedToleranceMeters(ShooterSpeeds.AUTO_SPEAKER), new WaitCommand(0),
+                new ShooterFlywheelSpeedInstantMeters(ShooterSpeeds.AUTO_SPEAKER), new WaitCommand(0),
                 new ShooterTrackPercentInstant(1));
     }
 }
