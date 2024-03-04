8a5ff88b127a2f6267cc47eadfa175e9d36ffe6a
Aayush Kokate <aayushext@outlook.com>
2024-03-03 17:33:41 -0800

    Woking tag align for Speaker

--- src/main/java/frc/robot/commands/vision/TagAlign.java
+++ src/main/java/frc/robot/commands/vision/TagAlign.java
@@ -2,52 +2,57 @@ package frc.robot.commands.vision;
 
 import org.photonvision.targeting.PhotonTrackedTarget;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.constants.ArmPositions;
+import frc.robot.constants.ShooterSpeeds;
+import frc.robot.subsystems.ReverseKinematics;
 import frc.robot.subsystems.swervedrive.SwerveSubsystem;
 import frc.robot.subsystems.vision.VisionEstimation;
 
 public class TagAlign extends Command {
     private VisionEstimation vision;
     private SwerveSubsystem swerveSubsystem;
-    private PhotonTrackedTarget cameraToTarget;
-    private double ySpeed = 0;
+    ChassisSpeeds realSpeeds;
+    Pose2d realPose2d;
+    Pose2d target;
+    double ySpeed;
 
     public TagAlign() {
         vision = VisionEstimation.getInstance();
         swerveSubsystem = SwerveSubsystem.getInstance();
     }
 
-    @Override
-    public void initialize() {
-        if (vision.backRightEstimator.hasTargets()) {
-            if (vision.backRightEstimator.getLatestResults().getBestTarget() != null) {
-                cameraToTarget = vision.backRightEstimator.getLatestResults().getBestTarget();
-            } else {
-                end(true);
-            }
-        }
-    }
+    // @Override
+    // public void initialize() {
+    //     if (vision.backRightEstimator.hasTargets()) {
+    //         if (vision.backRightEstimator.getLatestResults().getBestTarget() != null) {
+    //             cameraToTarget = vision.backRightEstimator.getLatestResults().getBestTarget();
+    //         } else {
+    //             end(true);
+    //         }
+    //     }
+    // }
 
     @Override
     public void execute() {
         try {
-            if (vision.backRightEstimator.hasTargets()) {
-                var fiducialId = vision.backRightEstimator.getLatestResults().getBestTarget().getFiducialId();
-                if (vision.backRightEstimator.getLatestResults().getBestTarget() != null
-                        && (fiducialId == 5 || fiducialId == 6)) {
-                    vision.backRightEstimator.getLatestResults().getBestTarget();
-                    ySpeed = (cameraToTarget.getBestCameraToTarget().getX() - 1.5) * 0.1;
+            realPose2d = swerveSubsystem.getPose();
+            realSpeeds = swerveSubsystem.getFieldVelocity();
+            target = vision.convert2dCoords(realPose2d);
+            ySpeed = -(target.getY() * 3);
 
-                } else {
-                    ySpeed = 0;
-                }
-
-                swerveSubsystem.drive(new Translation2d(swerveSubsystem.getRobotVelocity().vxMetersPerSecond, ySpeed),
-                        0, true);
-            }
+            SmartDashboard.putNumber("tagAlignTargetX", target.getX());
+            SmartDashboard.putNumber("tagAlignTargetY", target.getY());
+            SmartDashboard.putNumber("ySpeed", ySpeed);
+            //Replace to not use drive
+            swerveSubsystem.drive(new Translation2d(realSpeeds.vxMetersPerSecond, ySpeed), 0,
+                    true);
         } catch (Exception e) {
             //
         }
