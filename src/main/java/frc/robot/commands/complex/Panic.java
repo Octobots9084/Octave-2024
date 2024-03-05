e1afbf2ecb8e39917fccf191b076776c5558b416
Anemone <87347339+Anniemone@users.noreply.github.com>
2024-03-05 14:51:28 -0800

    Panic update

--- src/main/java/frc/robot/commands/complex/Panic.java
+++ src/main/java/frc/robot/commands/complex/Panic.java
@@ -3,12 +3,16 @@ package frc.robot.commands.complex;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.commands.arm.ShooterFlywheelSpeedInstantForce;
 import frc.robot.commands.arm.ShooterTrackSpeedInstantForce;
+import frc.robot.commands.intake.IntakeRollerSpeedInstant;
+import frc.robot.constants.IntakeSpeeds;
 import frc.robot.constants.ShooterSpeeds;
 
 public class Panic extends ParallelCommandGroup {
     public Panic() {
         addCommands(
                 new ShooterFlywheelSpeedInstantForce(ShooterSpeeds.PANIC),
+                new IntakeRollerSpeedInstant(IntakeSpeeds.PANIC),
+                new IntakeRollerSpeedInstant(IntakeSpeeds.PANIC),
                 new ShooterTrackSpeedInstantForce(ShooterSpeeds.PANIC));
     }
 }
