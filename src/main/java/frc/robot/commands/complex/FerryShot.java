1c225cd6ac54f54a6ea159155a47aaf5051fa792
Anemone <87347339+Anniemone@users.noreply.github.com>
2024-03-05 16:51:13 -0800

    ferry shot complete

--- src/main/java/frc/robot/commands/complex/FerryShot.java
+++ src/main/java/frc/robot/commands/complex/FerryShot.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.arm.ShooterElevatorPosInstant;
 import frc.robot.commands.arm.ShooterElevatorPosTolerance;
 import frc.robot.commands.arm.ShooterFlywheelSpeedInstant;
+import frc.robot.commands.arm.ShooterFlywheelSpeedInstantMeters;
 import frc.robot.commands.arm.ShooterPivotPosTolerance;
 import frc.robot.commands.arm.ShooterTrackSpeedInstant;
 import frc.robot.constants.ArmPositions;
@@ -12,7 +13,7 @@ import frc.robot.constants.ShooterSpeeds;
 public class FerryShot extends SequentialCommandGroup{
     public FerryShot() {
         addCommands(
-            new ShooterFlywheelSpeedInstant(ShooterSpeeds.FERRY_SHOT),
+            new ShooterFlywheelSpeedInstantMeters(ShooterSpeeds.FERRY_SHOT),
             new ShooterElevatorPosInstant(ArmPositions.FERRY_SHOT),
             new ShooterPivotPosTolerance(ArmPositions.FERRY_SHOT).withTimeout(1),
             new ShooterTrackSpeedInstant(ShooterSpeeds.FERRY_SHOT)
