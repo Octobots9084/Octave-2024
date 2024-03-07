d0cfe24a2a69dfacbff8b35d8db700ca7918ff98
Anemone <87347339+Anniemone@users.noreply.github.com>
2024-03-06 17:59:35 -0800

    Fixed error

--- src/main/java/frc/robot/subsystems/ReverseKinematics.java
+++ src/main/java/frc/robot/subsystems/ReverseKinematics.java
@@ -4,6 +4,7 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Constants;
 
 // velocities are positive going towards the target and negative when moving away
 // all units should be in meters, m/s, rad, etc.
