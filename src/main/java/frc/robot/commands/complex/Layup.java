3fcf2672f805ae35aff4dae4edb58baddc52d375
Anemone <87347339+Anniemone@users.noreply.github.com>
2024-03-06 19:12:46 -0800

    Trap is slowly improving

--- src/main/java/frc/robot/commands/complex/Layup.java
+++ src/main/java/frc/robot/commands/complex/Layup.java
@@ -2,11 +2,13 @@ package frc.robot.commands.complex;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.climb.ClimbPosTolerance;
 import frc.robot.constants.ClimbPositions;
 import frc.robot.commands.arm.ShooterElevatorPosTolerance;
 import frc.robot.commands.arm.ShooterFlywheelSpeedInstant;
 import frc.robot.commands.arm.ShooterFlywheelSpeedInstantMeters;
+import frc.robot.commands.arm.ShooterPivotPosInstant;
 import frc.robot.commands.arm.ShooterPivotPosTolerance;
 import frc.robot.commands.arm.ShooterTrackSpeedInstant;
 import frc.robot.constants.ArmPositions;
@@ -17,11 +19,14 @@ public class Layup extends SequentialCommandGroup {
     public Layup() {
         addCommands(
                 new ShooterPivotPosTolerance(ArmPositions.TRAP_SEGUEAY),
-                new ShooterElevatorPosTolerance(ArmPositions.TRAP_SEGUEAY).withTimeout(5),
+                new ShooterElevatorPosTolerance(ArmPositions.LAYUP).withTimeout(5),
                 new InstantCommand(() -> ShooterFlywheel.getInstance().setFlywheelActive(false)),
                 new ClimbPosTolerance(ClimbPositions.DOWN).withTimeout(3),
                 new ShooterPivotPosTolerance(ArmPositions.LAYUP),
-                new ShooterTrackSpeedInstant(ShooterSpeeds.LAYUP)
+                new WaitCommand(0.25),
+                new ShooterTrackSpeedInstant(ShooterSpeeds.LAYUP),
+                new WaitCommand(3),
+                new ShooterPivotPosInstant(ArmPositions.TRAP_SEGUEAY)
                 );
     }
 }
