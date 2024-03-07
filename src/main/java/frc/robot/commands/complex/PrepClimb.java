c57aacb3acb244894dc53a15a0dc83b3f5f86b3c
Anemone <87347339+Anniemone@users.noreply.github.com>
2024-03-06 21:28:28 -0800

    Jaden certified competition code, except ferry shot

--- src/main/java/frc/robot/commands/complex/PrepClimb.java
+++ src/main/java/frc/robot/commands/complex/PrepClimb.java
@@ -3,7 +3,11 @@ package frc.robot.commands.complex;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.climb.ClimbPosTolerance;
+import frc.robot.constants.ArmPositions;
 import frc.robot.constants.ClimbPositions;
+import frc.robot.subsystems.ShooterElevator;
+import frc.robot.subsystems.ShooterFlywheel;
+import frc.robot.subsystems.ShooterPivot;
 import frc.robot.subsystems.lights.Animations;
 import frc.robot.subsystems.lights.Light;
 
@@ -13,6 +17,14 @@ public class PrepClimb extends SequentialCommandGroup {
                 new ClimbPosTolerance(ClimbPositions.UP).withTimeout(10),
                 new InstantCommand(() -> {
                     Light.getInstance().setAnimation(Animations.CLIMB);
-                }));
+                }),new SequentialCommandGroup(new InstantCommand(()->{
+                ShooterElevator.getInstance().setPosition(ArmPositions.PREP_TRAP);
+            }),
+            new InstantCommand(()->{
+                ShooterPivot.getInstance().setPosition(ArmPositions.PREP_TRAP);
+            }),
+            new InstantCommand(()->{
+                ShooterFlywheel.getInstance().setFlywheelSpeed(0);
+            })));
     }
 }
