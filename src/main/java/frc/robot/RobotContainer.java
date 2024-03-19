6dbfe91cb7eef6e94dbdfe070fd1561c1069855a
Anemone <87347339+Anniemone@users.noreply.github.com>
2024-03-18 17:45:12 -0700

    wait after shot

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -85,7 +86,7 @@ public class RobotContainer {
                         NamedCommands.registerCommand("Collect", new CollectAuto());
 
                         NamedCommands.registerCommand("QuickDraw",
-                                        new DrivebyAuto(true).withTimeout(1));
+                                        new DrivebyAuto(true).withTimeout(1).andThen(new WaitCommand(0.1)));
 
                         NamedCommands.registerCommand("Shoot",
                                         new DrivebyAuto(false).withTimeout(2).andThen(new TheBigYeet()));
