ad0fdc1b316ec077923c630856ff54693c235c80
AlexanderGardiner <misc@alexandergardiner.com>
2024-03-22 13:53:28 -0700

    fixes

--- src/main/java/frc/robot/subsystems/ReverseKinematics.java
+++ src/main/java/frc/robot/subsystems/ReverseKinematics.java
@@ -13,7 +13,7 @@ import frc.robot.Constants;
 
 public class ReverseKinematics {
         // distance between launcher opening and the subwoofer target
-        private static double constTargetHeightDiff = 1.36;
+        private static double constTargetHeightDiff = 1.41;
         // gravity
         private static double g = 9.8;
         // the final y velocity for the note to be moving at when it enters the target
@@ -146,7 +146,7 @@ public class ReverseKinematics {
                 // if ((calcFerryDistance(pos, target))<7.1) {
                 // return 7.1;
                 // }
-                return ((calcFerryDistance(pos, target)) / 2.0 + 5.0)/2;
+                return ((calcFerryDistance(pos, target)) / 2.0 + 5.0) / 1.2;
         }
 
         public static double calcFerryLaunchAngle(Pose2d pos, Pose2d target) {
