65a27d831cd1b26b0ca76cfc3029c7075605be69
AlexanderGardiner <misc@alexandergardiner.com>
2024-03-23 09:52:57 -0700

    trap

--- src/main/java/frc/robot/commands/complex/Layup.java
+++ src/main/java/frc/robot/commands/complex/Layup.java
@@ -12,14 +12,15 @@ import frc.robot.commands.arm.ShooterTrackSpeedInstant;
 import frc.robot.constants.ArmPositions;
 import frc.robot.constants.ShooterSpeeds;
 import frc.robot.subsystems.ShooterFlywheel;
+import frc.robot.subsystems.ShooterPivot;
 import frc.robot.subsystems.lights.Animations;
 import frc.robot.subsystems.lights.Light;
 
 public class Layup extends SequentialCommandGroup {
     public Layup() {
         addCommands(new InstantCommand(() -> {
-                    Light.getInstance().setAnimation(Animations.CLIMB);
-                }),
+            Light.getInstance().setAnimation(Animations.CLIMB);
+        }),
                 new InstantCommand(() -> {
                     System.out.println("Layup requested");
                 }),
@@ -28,13 +29,20 @@ public class Layup extends SequentialCommandGroup {
                 new InstantCommand(() -> ShooterFlywheel.getInstance().setFlywheelActive(false)),
                 new ClimbPosTolerance(ClimbPositions.LAYUP).withTimeout(3),
                 new ShooterPivotPosTolerance(ArmPositions.LAYUP),
-                new WaitCommand(1),
+                new WaitCommand(2),
                 new ShooterTrackSpeedInstant(ShooterSpeeds.LAYUP),
-                new WaitCommand(3),
+                new WaitCommand(1),
+                new InstantCommand(() -> {
+                    ShooterPivot.getInstance().setPosition(ArmPositions.LAYUP.pivot - 0.009);
+                }),
+                new WaitCommand(0.2),
+                new InstantCommand(() -> {
+                    ShooterPivot.getInstance().setPosition(ArmPositions.LAYUP.pivot);
+                }),
+                new WaitCommand(1),
                 new ShooterPivotPosInstant(ArmPositions.TRAP_SEGUEAY),
                 new InstantCommand(() -> {
                     System.out.println("Layup completed");
-                })
-                );
+                }));
     }
 }
