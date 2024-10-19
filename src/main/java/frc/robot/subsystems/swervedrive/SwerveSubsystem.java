07f6c48fc1342e6323b338a3b9d1b647a2b1fa62
Aayush Kokate <aayushext@outlook.com>
2024-10-18 20:48:20 -0700

    lowered speed for swerve

--- src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -48,7 +48,7 @@ public class SwerveSubsystem extends SubsystemBase {
   /**
    * Maximum speed of the robot in meters per second, used to limit acceleration.
    */
-  public static final double MAXIMUM_SPEED = 6.5; // theotrical max speed is 6.85
+  public static final double MAXIMUM_SPEED = 6; // theotrical max speed is 6.85
   public Rotation2d targetAngle = new Rotation2d();
   public boolean targetAngleEnabled = false;
   public PIDController targetAngleController;
